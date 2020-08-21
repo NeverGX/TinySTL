42b135ac5b665cc489fc18fa38acc87a51ccdb04
wkh <15058715723@163.com>
2020-08-21 17:14:45 +0800

    the 5th commit

--- construct.h
+++ construct.h
@@ -6,7 +6,7 @@ namespace TinySTL{
     template<class T1, class T2>
     inline void construct(T1*p,const T2& value)
     {
-        new(p)T1(value); //placement new调用构造函数T1:T1(value)
+        new(p)T1(value); //placement new调用拷贝构造函数T1:T1(value)
     }
     template<class T>
     inline void destroy(T *p)
